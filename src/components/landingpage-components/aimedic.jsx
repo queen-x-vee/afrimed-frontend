bec5f2f706656dd427a568ce328fd6bf49630a78
valentina buoro <valentinabuoro4@gmail.com>
2024-02-19 11:30:57 +0100

    wip

--- src/components/landingpage-components/aimedic.jsx
+++ src/components/landingpage-components/aimedic.jsx
@@ -5,21 +5,21 @@ const Medic = () => {
     <div className='overflow-x-hidden w-full' >
     <div></div>
    <div className=''>
-   <div className='h-[15rem] bg-[#5D34F3] rotate-[-3.5deg] my-14 w-[150%] absolute -left-[19%]  flex justify-around items-center'>
-   <div className='  flex  '>
-   <div className='flex flex-col mx-7'>
-        <span className='font-semibold text-3xl text-[#F8F8F8]'> 1500+</span>
-        <span className='text-[#F8F8F8] text-sm'> Online Consultation</span>
+   <div className='h-[15rem] bg-[#5D34F3] rotate-[-3.5deg] my-14 w-[150%] absolute -left-[19%]  flex justify-center items-center'>
+   <div className=' w-11/12 flex items-around justify-center '>
+   <div className='flex flex-col mx-10'>
+        <span className='font-semibold text-3xl text-[#F8F8F8]'> 300+</span>
+        <span className='text-[#F8F8F8] text-sm'> Verified Specialist</span>
     </div>
-    <div className='flex flex-col mx-7'>
-        <span className='font-semibold text-3xl text-[#F8F8F8]'> 1500+</span>
-        <span className='text-[#F8F8F8] text-sm'> Online Consultation</span>
+    <div className='flex flex-col mx-10'>
+        <span className='font-semibold text-3xl text-[#F8F8F8]'> 1200+</span>
+        <span className='text-[#F8F8F8] text-sm'> Happy Costumers</span>
     </div>
-    <div className='flex flex-col mx-7'>
-        <span className='font-semibold text-3xl text-[#F8F8F8]'> 1500+</span>
-        <span className='text-[#F8F8F8] text-sm'> Online Consultation</span>
+    <div className='flex flex-col mx-10'>
+        <span className='font-semibold text-3xl text-[#F8F8F8]'> 80%</span>
+        <span className='text-[#F8F8F8] text-sm'> Happy Costumers</span>
     </div>
-    <div className='flex flex-col mx-7'>
+    <div className='flex flex-col mx-10'>
         <span className='font-semibold text-3xl text-[#F8F8F8]'> 1500+</span>
         <span className='text-[#F8F8F8] text-sm'> Online Consultation</span>
     </div>
