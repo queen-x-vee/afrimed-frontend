106d133119601a870537ced4c8159b6ea29fbae6
valentina buoro <valentinabuoro4@gmail.com>
2024-02-19 13:09:43 +0100

    wip

--- src/components/landingpage-components/footer.jsx
+++ src/components/landingpage-components/footer.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Logo from "../../assests/authentication/logo.svg";
 
 const Footer = () => {
   return (
@@ -13,9 +14,43 @@ const Footer = () => {
           </div>
         </div>
       </div>
-      <div className="bg-[#221B3B]">
+      <div className="bg-[#221B3B] py-4 px-10 md:py-8 md:px-20  ">
+       
+        <div className="flex flex-col items-center justify-center md:flex-row md:justify-between">
+        <div>
+          <img src={Logo} alt ='logo'/>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-10 mt-10">
+          <div className="flex flex-col gap-3 text-white text-sm">
+            <span className="font-bold">Services</span>
+            <span className="font-normal">AI-Powered Chatbot (AI Medic)</span>
+            <span className="font-normal">Virtual Consultations</span>
+            <span className="font-normal">Appointment Management</span>
+            <span className="font-normal">Secure Messaging</span>
+          </div>
+          <div className="flex flex-col gap-3 text-white text-sm">
+            <span  className="font-bold">Legal</span>
+            <span className="font-normal">Terms and conditions</span>
+            <span className="font-normal">Privacy</span>
+            <span className="font-normal">Support</span>
+          </div>
+          <div className="flex flex-col gap-3 text-white text-sm"> 
+            <span  className="font-bold">Contact Us</span>
+            <span className="font-normal">Email: afrimed@gmail.com</span>
+            <span className="font-normal">Contact: 08000098967</span>
+          </div>
+        </div>
+        </div>
+       
 
+        <hr className="border border-[#D9D9D9] mt-8 md:mt-10 mb-6" />
+        <div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
       </div>
+     
     </div>
   );
 };
