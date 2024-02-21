4d1ecef7fedc9657e342aa9cf1dbad48817eacf3
valentina buoro <valentinabuoro4@gmail.com>
2024-02-21 22:42:23 +0100

    push

--- src/patients-app/messages.jsx
+++ src/patients-app/messages.jsx
@@ -136,12 +136,6 @@ function MessagePage() {
          
 
           .message-input button {
-            margin-left: 10px;
-            padding: 10px 20px;
-            background-color: #007bff;
-            color: white;
-            border: none;
-            border-radius: 5px;
             cursor: pointer;
           }
 
