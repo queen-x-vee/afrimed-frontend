444e658f387979dbd35d80118b7f0819df27fb8a
valentina buoro <valentinabuoro4@gmail.com>
2024-02-21 23:03:41 +0100

    push

--- src/patients-app/doctors.jsx
+++ src/patients-app/doctors.jsx
@@ -1,29 +1,35 @@
 import React from "react";
 import PatientNavbar from "../components/patients-components/PatientNavbar";
 import PatientTopNavbar from "../components/patients-components/PatientTopNavbar";
+import Video from "../assests/patients-dashboard/video.svg";
+import Phone from "../assests/patients-dashboard/phone.svg";
+import Message from "../assests/patients-dashboard/message.svg";
 
 const recentPatients = [
   {
     patientName: "paul williams",
     visitID: "1",
     date: "12/12/2021",
-    gender: "male",
+    email: "doc@gmail.com",
     disease: "Heart attack",
-    status: "Completed",
-    action: "Completed",
+    phone: "0712345678",
+    availability: true
   },
   {
     patientName: "paul williams",
     visitID: "2",
+    email: "doc@gmail.com",
+    phone: "0712345678",
     date: "12/12/2021",
     gender: "male",
     disease: "Heart attack",
-    status: "Completed",
-    action: "Completed",
+    availability: true,
   },
   {
     patientName: "paul williams",
     visitID: "3",
+    email: "doc@gmail.com",
+    phone: "0712345678",
     date: "12/12/2021",
     gender: "male",
     disease: "Heart attack",
@@ -33,15 +39,15 @@ const recentPatients = [
   {
     patientName: "paul williams",
     visitID: "4",
-    date: "12/12/2021",
-    gender: "male",
-    disease: "Heart attack",
+    email: "doc@gmail.com",
+    phone: "0712345678",
     status: "Completed",
     action: "Completed",
   },
   {
     patientName: "paul williams",
     visitID: "5",
+    email: "doc@gmail.com",
     date: "12/12/2021",
     gender: "male",
     disease: "Heart attack",
@@ -51,59 +57,87 @@ const recentPatients = [
 ];
 
 const Doctors = () => {
+  const [availability, setAvailability] = React.useState(true);
+  
+
+  const declineAppointment = () => {
+    setAvailability(false);
+   
+  };
+  const acceptAppointment = () => {
+    setAvailability(true);
+    
+  };
   return (
     <>
       <div className=" h-full flex m-0 p-0">
         <PatientNavbar />
         <main className="md:ml-[20rem] w-full bg-[#F7F7F7] ">
-          <PatientTopNavbar/>
-        <div className="px-4 ">
-          
-         <div>
-         <div className="p-4  ">
-          <div className="grid grid-cols-6 my-7 gap-x-6  mb-2.5 bg-[#EBEFF2] p-6 font-medium text-lg text-[#696969]">
-            <span className="flex  justify-center items-center">Names</span>
-            <span className="flex  justify-center items-center"> ID</span>
-            <span className="flex  justify-center items-center">Email</span>
-            <span className="flex  justify-center items-center">Phone Number</span>
-            <span className="flex  justify-center items-center">Availability</span>
-            <span className="flex  justify-center items-center">Consult</span>
-           
-          </div>
-            {recentPatients.map((patient) => {
-              return (
-                <div
-                  key={patient.visitID}
-                  className="grid grid-cols-7 text-[#696969] font-normal  bg-white p-6 mb-2.5 "
-                >
+          <PatientTopNavbar />
+          <div className="px-4 ">
+            <div>
+              <div className="p-4  ">
+                <div className="grid grid-cols-6 my-7 gap-x-6  mb-2.5 bg-[#EBEFF2] p-6 font-medium text-lg text-[#696969]">
                   <span className="flex  justify-center items-center">
-                    {patient.patientName}
+                    Names
                   </span>
-                  
+                  <span className="flex  justify-center items-center"> ID</span>
                   <span className="flex  justify-center items-center">
-                    {patient.visitID}
-                  </span>
-
-                  <span className="flex  justify-center items-center">
-                    {patient.date}
+                    Email
                   </span>
                   <span className="flex  justify-center items-center">
-                    {patient.gender}
+                    Phone Number
                   </span>
                   <span className="flex  justify-center items-center">
-                    {patient.disease}
+                    Availability
                   </span>
                   <span className="flex  justify-center items-center">
-                    {patient.status}
+                    Consult
                   </span>
-                 
                 </div>
-              );
-            })}
+                {recentPatients.map((patient) => {
+                  return (
+                    <div
+                      key={patient.visitID}
+                      className="grid grid-cols-6 gap-x-6 text-[#696969] font-normal  bg-white p-6 mb-2.5 "
+                    >
+                      <span className="flex  justify-center items-center">
+                        {patient.patientName}
+                      </span>
+
+                      <span className="flex  justify-center items-center">
+                        {patient.visitID}
+                      </span>
+
+                      <span className="flex  justify-center items-center">
+                        {patient.email}
+                      </span>
+                      <span className="flex  justify-center items-center">
+                        {patient.phone}
+                      </span>
+                      <span className="flex  justify-center items-center">
+                      <div
+                                className={
+                                  availability
+                                    ? "bg-[#DDFFEC] text-[#0A9D4C] py-1.5 px-2.5 rounded "
+                                    : "bg-[#FEEEEF] text-[#FF5363] py-1.5 px-2.5 rounded"
+                                }
+                              >
+                                {availability ? "Accepted" : "Declined"}
+                              </div>
+                      </span>
+                      <span className="flex  justify-between items-center">
+                        <img src={Video} alt="video" />
+                        <img src={Phone} alt="phone" />
+                        <img src={Message} alt="message" />
+                      </span>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+            <div className="bg-white"></div>
           </div>
-         </div>
-          <div className="bg-white"></div>
-        </div>
         </main>
       </div>
     </>
