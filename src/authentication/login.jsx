89b3a4be6ef82953f8f2da6352b529175691a19d
Augustus Shaminga <shamingaa@gmail.com>
2024-02-22 12:49:34 +0100

    Add login functionality for both users

--- src/authentication/login.jsx
+++ src/authentication/login.jsx
@@ -1,10 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "../assests/authentication/logo.svg";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import useAuthStore from "../stores/authStore";
+import Loader from "../components/Loader";
+import MessageAlert from "../components/MessageAlert";
 
 const Login = () => {
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+  const [showMessage, setShowMessage] = useState(false);
+  const [message, setMessage] = useState("");
+  const [messageID, setMessageID] = useState("");
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    try {
+      const res = await (
+        await fetch(`https://api-afrimed.vercel.app/api/auth/otp/generate/`, {
+          method: "POST",
+          headers: {
+            "content-type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        })
+      ).json();
+
+      if (!res.success) {
+        throw res;
+      }
+
+      navigate("/verify");
+      setLoading(false);
+    } catch (error) {
+      setMessage(Object.values(error)[0]);
+      setMessageID("declineAlert");
+      setShowMessage(true);
+      setLoading(false);
+    }
+  };
+
   return (
     <div className=" w-full ">
+      {/* ------ Alert Component ---------- */}
+      {showMessage ? (
+        <MessageAlert
+          data={{ message, messageID }}
+          onDurationChange={setTimeout(() => {
+            setShowMessage(false);
+          }, 2000)}
+        />
+      ) : (
+        ""
+      )}
+
       <div className=" background-image w-full  flex justify-center items-center  py-10 md:py-20">
         <div className="sub-text bg-white px-6 md:px-14 pt-10 pb-9 shadow-xl mx-auto w-full max-w-lg rounded-2xl ">
           <div className=" mx-auto flex w-full max-w-md flex-col space-y-16 ">
@@ -12,9 +74,7 @@ const Login = () => {
               <img src={Logo} alt="logo" className="w-32" />
             </div>
             <div className="flex flex-col items-center justify-center text-center space-y-2">
-              <div
-                className="font-medium text-2xl text-[#272728]"
-              >
+              <div className="font-medium text-2xl text-[#272728]">
                 <p>Welcome</p>
               </div>
               <div className="flex flex-row text-base font-normal text-[#575758]">
@@ -23,7 +83,7 @@ const Login = () => {
             </div>
 
             <div>
-              <form>
+              <form onSubmit={handleSubmit}>
                 <div className="flex flex-col space-y-10 mb-10">
                   <div className="flex justify-between p-4 w-full  rounded-xl bg-gray-100">
                     <input
@@ -31,37 +91,49 @@ const Login = () => {
                       placeholder="Enter email"
                       id="email"
                       name="email"
+                      value={formData.email}
+                      onChange={handleChange}
                     />
                   </div>
 
                   <div className="flex justify-between p-4 w-full  rounded-xl bg-gray-100">
                     <input
+                      type="password"
                       className="bg-inherit w-11/12 border-none outline-none"
                       placeholder="Enter password"
                       id="password"
                       name="password"
+                      value={formData.password}
+                      onChange={handleChange}
                     />
                   </div>
                   <div className="flex items-start">
-                    <span className="font-normal text-xs text-[#FF2C2C]">Forgot Password?</span>
+                    <span className="font-normal text-xs text-[#FF2C2C]">
+                      Forgot Password?
+                    </span>
                   </div>
                 </div>
                 <div className="flex flex-col space-y-4 mb-10">
-                  <button className="flex items-center p-4 justify-center text-white text-xs w-full  rounded-xl bg-[#5D34F3]">
-                    
-                      <Link to='/patient'>Sign in as a Patient</Link>
-                    
+                  <button
+                    type="submit"
+                    className="flex items-center p-4 justify-center text-white text-xs w-full  rounded-xl bg-[#5D34F3]"
+                  >
+                    {loading ? <Loader /> : "Sign in"}
                   </button>
-                  <button className="flex items-center p-4 justify-center text-black text-xs w-full  rounded-xl border border-[#5D34F3]">
-                    <Link to='/doctor'>Sign in as a Doctor</Link>
-                  </button>
+
+                  {/* <button className="flex items-center p-4 justify-center text-black text-xs w-full  rounded-xl border border-[#5D34F3]">
+                    <Link to="/doctor">Sign in as a Doctor</Link>
+                  </button> */}
                 </div>
               </form>
             </div>
           </div>
           <div className="font-normal text-xs">
             <span>Don't have an account?</span>
-            <Link to="/signup" className="text-[#5D34F3]"> Create Account</Link>
+            <Link to="/signup" className="text-[#5D34F3]">
+              {" "}
+              Create Account
+            </Link>
           </div>
         </div>
       </div>
